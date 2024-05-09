ae5516cd19b58af389fbd72ca0669590c205d8f4
Isaac Okechukwu Chukwuma <okechukwuisaac111@gmail.com>
2024-05-10 00:28:38 +0100

    all done

---
+++ src/components/Newsletter.js
@@ -0,0 +1,46 @@
+import { Alert } from "react-bootstrap"
+import { useState, useEffect } from "react"
+import { Row, Col } from "react-bootstrap";
+export const Newsletter = ({ onValidated, status, message }) => {
+    const [email, setEamil] = useState('');
+
+    useEffect(() => {
+        if (status === 'success') clearFileds();
+    }, [status])
+
+    const handleSubmit = (e) => {
+        e.preventDefault();
+        email &&
+        email.indexOf("@") > -1 &&
+        onValidated({
+            EMAIL: email
+        })
+    }
+    const clearFileds = () => {
+        setEamil('');
+    }
+
+
+    return (
+        <Col lg={12}>
+            <div className="newsletter-bx">
+                <Row>
+                    <Col lg={12} md={6} xl={5}>
+                        <h3>Subscribe to our Newsletter</h3>
+                        {status === 'sending' && <Alert>Sending.....</Alert>}
+                        {status === 'error' && <Alert variant="danger">{message}</Alert>}
+                        {status === 'success' && <Alert variant="success">{message}</Alert>}
+                    </Col>
+                    <Col md={6} xl={7}>
+                        <form onSubmit={handleSubmit}>
+                            <div className="new-email-bx">
+                                <input value={email} type="email" onChange={(e) => setEamil(e.target.value)} placeholder="Email Address" />
+                                <button type="submit">Submit</button>
+                            </div>
+                        </form>
+                    </Col>
+                </Row>
+            </div>
+        </Col>
+    )
+}
---
+++ src/components/Projects.js
@@ -0,0 +1,80 @@
+import {Col, Row, Container, Nav, Tab } from "react-bootstrap";
+import { ProjectCard } from "./ProjectCard";
+import colorSharp2 from "../assets/img/color-sharp2.png";
+import projImg1 from "../assets/img/project-img1.png"
+import projImg2 from "../assets/img/project-img2.png"
+import projImg3 from "../assets/img/project-img3.png"
+export const Projects = () => {
+
+
+    const projects = [
+        {
+            title: "Business Startup",
+            description: "Design & Development",
+            imgUrl: projImg1,
+        },
+        {
+            title: "Business Startup",
+            description: "Design & Development",
+            imgUrl: projImg2,
+        },
+        {
+            title: "Business Startup",
+            description: "Design & Development",
+            imgUrl: projImg3,
+        },
+        {
+            title: "Business Startup",
+            description: "Design & Development",
+            imgUrl: projImg1,
+        },
+    ];
+
+    return (
+        <section className="project" id="project">
+            <Container>
+                <Row>
+                    <Col>
+                    <h2>Projects</h2>
+                    <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum</p>
+                    <Tab.Container id="project-tabs" defaultActiveKey="first">
+                    <Nav variant="pills" className="nav-pills mb-5 justify-content-center align-items-center" id="pills-tab">
+                        <Nav.Item>
+                            <Nav.Link href="first">Tab One</Nav.Link>
+                        </Nav.Item>
+                        <Nav.Item>
+                            <Nav.Link eventKey="second">Tab Two</Nav.Link>
+                        </Nav.Item>
+                        <Nav.Item>
+                            <Nav.Link eventKey="third">
+                              Tab Three
+                            </Nav.Link>
+                        </Nav.Item>
+                    </Nav>
+                    <Tab.Content>
+                        <Tab.Pane eventKey="first">
+                            <Row>
+                                {
+                                    projects.map((project, index) => {
+                                        return (
+                                            <ProjectCard
+                                            key={index}
+                                            {...project}
+                                            />
+                                        )
+                                    })
+                                }
+                            </Row>
+                        </Tab.Pane>
+                        <Tab.Pane eventKey="second">Lorem Ipsums</Tab.Pane>
+                        <Tab.Pane eventKey="third">Lorem Ipsums</Tab.Pane>
+                    </Tab.Content>
+                    </Tab.Container>
+                    </Col>
+                </Row>
+            </Container>
+            <img className="background-image-right" src={colorSharp2}></img>
+
+        </section>
+    )
+}
