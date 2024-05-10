eae2eb2358a390231427a796a118f2301ff42efc
Isaac Okechukwu Chukwuma <okechukwuisaac111@gmail.com>
2024-05-10 02:00:38 +0100

    done

--- src/components/MailchimpForm.js
+++ src/components/MailchimpForm.js
@@ -1,22 +1,22 @@
-import MailchimpSubscribe from "react-mailchimp-subscribe"
-import { Newsletter } from "./Newsletter"
+import MailchimpSubscribe from "react-mailchimp-subscribe";
+import { Newsletter } from "./Newsletter";
 
-export  const MailchimpForm = () => {
-    const postUrl = `${process.env.REACT_APP_MAILCHIMP_URL}?u=${process.env.REACT_APP_MAILCHIMP_U}$id=${process.env.REACT_APP_MAILCHIMP_ID}`
+export const MailchimpForm = () => {
+//   const postUrl = `${process.env.REACT_APP_MAILCHIMP_URL}?u=${process.env.REACT_APP_MAILCHIMP_U}&id=${process.env.REACT_APP_MAILCHIMP_ID}`;
+const postUrl = "";
 
-    return (
-        <>
-        <MailchimpSubscribe
+  return (
+    <>
+      <MailchimpSubscribe
         url={postUrl}
-        render={({ subscribe, status, message }) => {
-            <Newsletter
+        render={({ subscribe, status, message }) => (
+          <Newsletter
             status={status}
             message={message}
             onValidated={formData => subscribe(formData)}
             />
-        }}
+        )}
         />
-        
-        </>
-    )
+    </>
+  )
 }
--- src/components/Newsletter.js
+++ src/components/Newsletter.js
@@ -1,46 +1,46 @@
-import { Alert } from "react-bootstrap"
-import { useState, useEffect } from "react"
-import { Row, Col } from "react-bootstrap";
-export const Newsletter = ({ onValidated, status, message }) => {
-    const [email, setEamil] = useState('');
+import { useState, useEffect } from "react";
+import { Col, Row, Alert } from "react-bootstrap";
 
-    useEffect(() => {
-        if (status === 'success') clearFileds();
-    }, [status])
+export const Newsletter = ({ status, message, onValidated }) => {
+  const [email, setEmail] = useState('');
 
-    const handleSubmit = (e) => {
-        e.preventDefault();
-        email &&
-        email.indexOf("@") > -1 &&
-        onValidated({
-            EMAIL: email
-        })
-    }
-    const clearFileds = () => {
-        setEamil('');
-    }
+  useEffect(() => {
+    if (status === 'success') clearFields();
+  }, [status])
 
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    email &&
+    email.indexOf("@") > -1 &&
+    onValidated({
+      EMAIL: email
+    })
+  }
 
-    return (
-        <Col lg={12}>
-            <div className="newsletter-bx">
-                <Row>
-                    <Col lg={12} md={6} xl={5}>
-                        <h3>Subscribe to our Newsletter</h3>
-                        {status === 'sending' && <Alert>Sending.....</Alert>}
-                        {status === 'error' && <Alert variant="danger">{message}</Alert>}
-                        {status === 'success' && <Alert variant="success">{message}</Alert>}
-                    </Col>
-                    <Col md={6} xl={7}>
-                        <form onSubmit={handleSubmit}>
-                            <div className="new-email-bx">
-                                <input value={email} type="email" onChange={(e) => setEamil(e.target.value)} placeholder="Email Address" />
-                                <button type="submit">Submit</button>
-                            </div>
-                        </form>
-                    </Col>
-                </Row>
-            </div>
-        </Col>
-    )
+  const clearFields = () => {
+    setEmail('');
+  }
+
+  return (
+      <Col lg={12}>
+        <div className="newsletter-bx wow slideInUp">
+          <Row>
+            <Col lg={12} md={6} xl={5}>
+              <h3>Subscribe to our Newsletter<br></br> & Never miss latest updates</h3>
+              {status === 'sending' && <Alert>Sending...</Alert>}
+              {status === 'error' && <Alert variant="danger">{message}</Alert>}
+              {status === 'success' && <Alert variant="success">{message}</Alert>}
+            </Col>
+            <Col md={6} xl={7}>
+              <form onSubmit={handleSubmit}>
+                <div className="new-email-bx">
+                  <input value={email} type="email" onChange={(e) => setEmail(e.target.value)} placeholder="Email Address" />
+                  <button type="submit">Submit</button>
+                </div>
+              </form>
+            </Col>
+          </Row>
+        </div>
+      </Col>
+  )
 }
